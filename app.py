67cc5b424c971125044fa32e25277c3712292856
solucionai <compartilhadosolucionai@gmail.com>
2024-10-31 14:54:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -230,12 +230,7 @@ def search():
     results = scrape_tjsp(term)
     return jsonify(results)
 
+# if __name__ == "__main__":
+#     app.run_server(debug=True, host='0.0.0.0', port=8080)
 if __name__ == '__main__':
     app.run()
-
-
-# In[ ]:
-
-
-
-
